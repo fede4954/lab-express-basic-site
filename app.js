dbf746f9aafa6851ef39abc35393b1c6a82b6d97
Fede <f.gomezlara@outlook.es>
2021-11-08 17:10:48 +0100

    routing

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+//Routes
+app.get('/', (_, response) => {
+    response.sendFile('index.html')
+})
+
+app.get('/about', (_, response) => {
+    response.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (_, response) => {
+    response.sendFile(__dirname + '/views/works.html')
+})
+
+app.listen(3000, () => { 
+    console.log('Server active in port 3000')
+})
+  
+  
+  
+
+  
